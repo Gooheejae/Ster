4c11e09a5a52525d41698624e0419268d805c37b
구희재 <goohij@gmail.com>
2015-12-29 20:39:25 +0900

    c 파일

--- HelloWorld.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-for i in range(0,6,1):
-	for j in range(0,i):
-		sys.stdout.write('*')
-	print("")
--- hello.py
+++
@@ -1,9 +0,0 @@
-print("HelloWorld")
-print("hellworld")
-print("nanana")
-<<<<<<< HEAD
-print("dd1233334dd")
-=======
-print("dd123dd")
->>>>>>> hotfix
-input()
